968f42b54ac855c73bd3e6b649943d2e764d32a7
Matthew Hendrix <hendrixm290@gmail.com>
2020-09-02 12:43:47 -0400

    Encapsulated

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount BankAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            BankAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you. Your balance is now {BankAccount.GetBalance()}");
+        }
+    }
+}
